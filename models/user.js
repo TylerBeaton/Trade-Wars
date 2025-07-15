77112663df493e7e95e80994926db19aaa3b6f31
Tyler Beaton <91505331+TylerBeaton@users.noreply.github.com>
2025-07-15 13:49:31 -0230

    Add user model and database integration with Sequelize
    - Updated `.gitignore` to include `MigrationBackup/` and `.env`.
    - Integrated `dotenv` for environment variable management in `app.js`.
    - Established a Sequelize connection to PostgreSQL and defined a `User` model.
    - Created `user.js` to define the `User` model with required fields.
    - Updated `package-lock.json` and `package.json` to include new dependencies: `dotenv`, `pg`, `pg-hstore`, and `sequelize`, along with `nodemon` for development.
    - Refactored `users.js` to implement user creation logic with validation and error handling.

---
+++ models/user.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+};
